5b181a62d6482a29e619705457210acb256b3f57
niteshprog <74867544+niteshprog@users.noreply.github.com>
2020-11-25 02:08:53 +0530

    Create practical1.c

---
+++ experiment 2/practical1.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	double a, b;
+	printf("\nEnter the first no.");
+	scanf("%lf", &a);
+	printf("\nEnter the second no.");
+	scanf("%lf", &b);
+	
+	printf("\naddition is %f",a+b);
+	printf("\nsubtraction is %f",a-b);
+	printf("\nmultiplication is %f",a*b);
+	printf("\ndivision is %f",a/b);
+	return 0;
+}
