a5a4b7c37c40f82ace72169bbba1e8e929e8e57b
niteshprog <74867544+niteshprog@users.noreply.github.com>
2020-11-25 16:48:35 +0530

    Create practical 2.c

---
+++ experiment 4/practical 2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{	
+    int year;
+	printf("Enter the year:");
+	scanf("%d", &year);
+	if(year%4==0){
+		printf("This year is leap year.");
+	}else{
+		printf("This year is not leap year.");
+	}
+	return 0;
+} 
