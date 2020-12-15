4266cef637aea1bea64967d825d0c3e07da57156
Nitesh <74867544+niteshprog@users.noreply.github.com>
2020-12-16 00:14:43 +0530

    Create practice 3.c

---
+++ experiment 5/practice 3.c
@@ -0,0 +1,19 @@
+/*divisble no by 3 & 5*/
+#include <stdio.h>
+#include <stdlib.h>
+ 
+
+int main( ){
+	int n,i,z, k;
+	printf("Enter the upper limit:");
+	scanf("%d", &n);
+	for(i=1;i<=n;i++){
+		z= i%5;
+		k= i%3;
+	if(z==0 && k==0){
+	printf("\n %d is divisible by three and five.", i);} 
+	}return 0;
+	
+}
+	
+	
