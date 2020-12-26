1aae29d242ec48f4a818b2e5e4a3d6a35f1b8237
Nitesh <74867544+niteshprog@users.noreply.github.com>
2020-12-26 19:13:42 +0530

    Create practical_1a.c

---
+++ experiment 6/practical_1a.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<math.h>
+void armstrong(int armno)
+{
+	int ac, digits,armsum=0;
+	digits=(int)log10(armno)+1;
+	ac= armno;
+	for(ac= armno; ac>0; ac/=10)
+	{
+	  int rem= ac%10;
+	   armsum+=pow(rem, digits);
+	   
+		}if (armsum==armno)
+		{
+			printf("This is armstrong no.");
+		}else{
+			printf("This is not armstrong no.");
+		}
+	
+    
+} 
+void main()
+{
+	printf("Let's check for the Armstrong number.\n");
+	int armno;
+	printf("Enter the number to check the armstrong number:\n");
+	scanf("%d", &armno);
+	armstrong(armno);
+}
